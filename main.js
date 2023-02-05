106cc884c71cf176a9010ada95381b8ccedada58
Yargen Gonzalez <y.gonzalez@correo.unimet.edu.ve>
2023-02-05 16:37:14 -0400

    cambios menores

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function validateEmail(email) {
 }
 
 ////////////////////////////-- HERO --///////////////////////////////!SECTION
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 function plusSlides(n) {
     showSlides(slideIndex += n);
@@ -80,9 +80,9 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("hero_img");
-    var dots = document.getElementsByClassName("mark");
+    let i;
+    let slides = document.getElementsByClassName("hero_img");
+    let dots = document.getElementsByClassName("mark");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
@@ -94,17 +94,6 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
-// Auto Slide   if you need auto slide ,remove the commit "//"
-//var slideIndex = 0;
-//showSlides();
-//function showSlides() {
-    //var i;
-  //  var slides = document.getElementsByClassName("mySlides");
-   // for (i = 0; i < slides.length; i++) {
-   //     slides[i].style.display = "none";
-   // }
-  //  slideIndex++;
-   // if (slideIndex > slides.length) { slideIndex = 1 }
-   // slides[slideIndex - 1].style.display = "block";
-   // setTimeout(showSlides, 2000); // Change image every 2 seconds
-//}
+
+
+
